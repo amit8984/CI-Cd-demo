f8a8d42c24b56259378693cc2006d93ed3d571b1
amit8984 <56793149+amit8984@users.noreply.github.com>
2020-03-19 15:27:33 +0530

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -2,4 +2,4 @@ use warehouse dev_engineer_wh;
 use edm_confirmed_dev;
 use schema scratch;
 
-create or replace table as select * from Banner where 1=2;
+create or replace table Banner_copy as select * from Banner where 1=2;
